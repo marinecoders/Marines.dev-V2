755656904823beb8a74416e6d19098c82444ed38
will <will@wcrum.dev>
2025-09-19 08:08:32 -0600

    updated image and verbiage

--- src/components/logos/McswfLogoAndText.svelte
+++ src/components/logos/McswfLogoAndText.svelte
@@ -8,11 +8,11 @@
   {#if displayImage}
     <img
       style="fill: white;"
-      src="/miu.png"
+      src="/miu.svg"
       class="h-16 text-white mr-4"
       alt="MIU Logo" />
   {/if}
   <span
     class="tracking-widest leading-normal sm:leading-snug text-[15px] sm:text-[21px] text-white font-colossalis whitespace-nowrap bg-gradient-to-r from-white via-slate-50 to-slate-100 inline-block text-transparent bg-clip-text"
-    >U.S. MARINE CORPS<br />INNOVATION UNIT</span>
+    >MARINE INNOVATION UNIT</span>
 </a>

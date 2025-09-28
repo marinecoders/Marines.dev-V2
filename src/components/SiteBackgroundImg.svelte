c0d40ee9efac7a5b1effaff035d925e2ea91acfc
will <will@wcrum.dev>
2025-09-28 01:23:49 -0600

    centered back image

--- src/components/SiteBackgroundImg.svelte
+++ src/components/SiteBackgroundImg.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="absolute top-0 w-screen h-full -z-20 dark:bg-[#252a34]" style="background-color: var(--bg-primary);">
-  <div class="fixed left-0 -z-10 flex justify-center w-full align-middle top-24 md:top-48 h-full">
+  <div class="fixed left-0 -z-10 flex justify-center w-full align-middle top-24 md:top-48 h-full mt-24">
     <MarineCodersLogo class="opacity-5 w-full dark:opacity-40" color="#1a1d24" EGAColor="#1a1d24" />
   </div>
 </div>

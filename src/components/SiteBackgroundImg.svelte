07388950183cef8e0797cd83ddaced919c7a7cd7
will <will@wcrum.dev>
2025-09-28 01:21:30 -0600

    update stuff

--- src/components/SiteBackgroundImg.svelte
+++ src/components/SiteBackgroundImg.svelte
@@ -3,7 +3,7 @@
   import { isDarkMode } from './stores.js';
 </script>
 
-<div class="absolute top-0 w-screen h-full -z-20" style="background-color: var(--bg-primary);">
+<div class="absolute top-0 w-screen h-full -z-20 dark:bg-[#252a34]" style="background-color: var(--bg-primary);">
   <div class="fixed left-0 -z-10 flex justify-center w-full align-middle top-24 md:top-48 h-full">
     <MarineCodersLogo class="opacity-5 w-full dark:opacity-40" color="#1a1d24" EGAColor="#1a1d24" />
   </div>

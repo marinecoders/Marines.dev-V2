8bee14c663d4c177e07fba48436a363746dcf8f9
will <will@wcrum.dev>
2025-11-19 11:30:50 -0700

    remove astro icon to fix build
    We are using iconify, but probably need to consolidate. :) please use dev branch

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import svelte from '@astrojs/svelte';
 
-import icon from 'astro-icon';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), mdx(), icon()],
+  integrations: [tailwind(), svelte(), mdx()],
   site: 'https://dev-miu.usmc.dev',
   base: '/',
   redirects: {

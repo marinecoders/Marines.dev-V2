afbd8fc5e7e900ae393477821d36e0f3bf49b5e5
will <30413278+wcrum@users.noreply.github.com>
2026-01-15 18:11:30 -0700

    update scroll bar and pub1 (#73)

--- src/components/SiteBackgroundImg.svelte
+++ src/components/SiteBackgroundImg.svelte
@@ -2,16 +2,19 @@
   import MarineCodersLogo from '@components/logos/MarineCodersLogo.svelte';
   import { isDarkMode } from './stores.js';
   import BackgroundBeams from '@components/effects/BackgroundBeams.svelte';
+  export let hideBackground = false;
 </script>
 
 <div
   class="absolute top-0 w-screen h-full -z-20 dark:bg-[#252a34]"
   style="background-color: var(--bg-primary);">
+  {#if !hideBackground}
   <div class="fixed left-0 -z-10 flex justify-center w-full align-middle top-24 md:top-48 h-full mt-24">
     <MarineCodersLogo
       class="opacity-40 w-full dark:opacity-40"
       color="#1a1d24"
       EGAColor="#1a1d24" />
   </div>
+  {/if}
   <BackgroundBeams className="pointer-events-none" />
 </div>

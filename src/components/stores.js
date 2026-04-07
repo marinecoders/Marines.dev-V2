042b6d155c5ef406ff7caf78930c6a817c71fe05
will <will@wcrum.dev>
2026-04-07 17:28:51 -0600

    fix: remove site banner
    Signed-off-by: will <will@wcrum.dev>

--- src/components/stores.js
+++ src/components/stores.js
@@ -3,7 +3,7 @@ import { writable } from 'svelte/store'
 export const mobile = writable(0)
 export const active = writable(0)
 
-export const displaySiteBanner = writable(false) // Show the site Banner.svelte component
+
 export const displaySiteClassificationBanner = writable(false) // Show the site ClassificationBanner.svelte component
 export const displaySiteOfficialBanner = writable(false) // Show the site OfficialBanner.svelte component
 
